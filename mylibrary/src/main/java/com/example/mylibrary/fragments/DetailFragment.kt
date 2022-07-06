194beceff481227e012025a00964e5708fcdcd1b
Hamza rafique <ameer.hamza6733@gmail.com>
2022-07-06 05:57:59 +0500

    making comment api call again on post detail  increase it fail at post list screen

--- mylibrary/src/main/java/com/example/mylibrary/fragments/DetailFragment.kt
+++ mylibrary/src/main/java/com/example/mylibrary/fragments/DetailFragment.kt
@@ -4,17 +4,22 @@ import android.os.Bundle
 import android.preference.PreferenceManager
 import android.view.View
 import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.core.content.ContextCompat
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.example.mylibrary.Log
 import com.example.mylibrary.R
+import com.example.mylibrary.Resorces
 import com.example.mylibrary.Util
 import com.example.mylibrary.adupters.PostDetailAdupter
 import com.example.mylibrary.databinding.FragmentDetailBinding
 import com.example.mylibrary.model.EventByUser
 import com.example.mylibrary.model.Meta
+import com.example.mylibrary.model.request.CommentPostRequest
 import com.example.mylibrary.repository.EventRepo
+import com.example.mylibrary.viewModel.DetailFragmentViewModel
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
@@ -32,6 +37,9 @@ class DetailFragment : BaseFragment(R.layout.fragment_detail), OnMapReadyCallbac
     @Inject
     lateinit var eventRepo:EventRepo
     private val binding by viewBinding(FragmentDetailBinding::bind)
+    private val viewModel by viewModels<DetailFragmentViewModel> ()
+    private var postDetailAdupter:PostDetailAdupter?=null
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -58,11 +66,35 @@ class DetailFragment : BaseFragment(R.layout.fragment_detail), OnMapReadyCallbac
                 val envent =EventByUser(EventByUser.TEST_APP_ID,postDetail.postUi.id.toString(),EventByUser.ACTION_VIEW,Util.id(requireContext().applicationContext).toString(),
                     Meta(System.currentTimeMillis(),Meta.TEST_GPS_LOCATION)
                 )
+                postDetailAdupter=PostDetailAdupter(postDetail)
                 eventRepo.saveEvent(events = envent)
                 binding.recyclerViewPostDetail.layoutManager=LinearLayoutManager(requireActivity())
-                binding.recyclerViewPostDetail.adapter=PostDetailAdupter(postDetail)
+                binding.recyclerViewPostDetail.adapter=postDetailAdupter
+
+                Log("${postDetail.postUi.author?.name}")
+                //incase we fail to download comment list in previous screen then force to downlaod here
+                if (postDetail.commentList==null || postDetail.commentList?.isEmpty()==true){
+                    viewModel.getPostComments(CommentPostRequest(postDetail.postUi.id))
+                }
             }
         }
+
+        viewModel.liveDataCommentReponse.observe(this,{
+            when(it){
+                is Resorces.Success->{
+                    binding.progress.visibility=View.INVISIBLE
+                    postDetailAdupter?.postDetailUi?.commentList=it.data.postCommentList
+                    postDetailAdupter?.notifyDataSetChanged()
+                }
+                is Resorces.Error->{
+                    binding.progress.visibility=View.INVISIBLE
+                  Toast.makeText(activity!!,"error while loaidng comments",Toast.LENGTH_LONG).show()
+                }
+                is Resorces.Loading->{
+                    binding.progress.visibility=View.VISIBLE
+                }
+            }
+        })
     }
 
 
---
+++ mylibrary/src/main/java/com/example/mylibrary/viewModel/DetailFragmentViewModel.kt
@@ -0,0 +1,33 @@
+package com.example.mylibrary.viewModel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.mylibrary.Resorces
+import com.example.mylibrary.model.request.CommentPostRequest
+import com.example.mylibrary.model.ui.CommetReponseUI
+import com.example.mylibrary.repository.PostRepo
+import com.example.mylibrary.repository.UserRepo
+import com.example.mylibrary.room.EventDataBase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+@HiltViewModel
+class DetailFragmentViewModel @Inject constructor(private val postRepo: PostRepo,
+                                                  private val userRepo: UserRepo,
+                                                  private val datbase : EventDataBase):ViewModel() {
+
+    private val _mutableCommentsLiveData: MutableLiveData<Resorces<CommetReponseUI>> = MutableLiveData()
+    val liveDataCommentReponse: LiveData<Resorces<CommetReponseUI>> = _mutableCommentsLiveData
+
+
+      fun getPostComments(commentPostRequest: CommentPostRequest){
+          _mutableCommentsLiveData.postValue(Resorces.Loading())
+        viewModelScope.launch (Dispatchers.IO){
+            val reponse=postRepo.getComment(commentPostRequest)
+            _mutableCommentsLiveData.postValue(reponse)
+        }
+    }
+}
